e045da05fec033ac22c1f63b34a3f36a1214830b
Tafita1339 <fabricep19.aps1a@gmail.com>
2021-11-04 07:03:50 -0700

    Composant de base

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        apiResponse: [
+            { name: 'GitHub', url: 'https://www.github.com' },
+            { name: 'Twitter', url: 'https://www.twitter.com' },
+            { name: 'Netlify', url: 'https://www.netlify.com' }
+        ]
+    }
+})
+
+console.log(Vue)
